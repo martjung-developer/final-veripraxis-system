19a61a2da0739fa6dd2624cfe8ca63c7b6fd461b
Emmanuel Martir <martirjulian12@gmail.com>
2026-06-30 05:37:41 +0800

    fix: force dynamic for login page

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -11,6 +11,8 @@
 // ─────────────────────────────────────────────────────────────────────────────
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useState, useEffect }     from 'react'
 import Image                       from 'next/image'
 import Link                        from 'next/link'
